a1e03ec34377340b993dbbffd618e86409821126
Elton Leite <borelanjo@gmail.com>
2020-05-23 20:55:52 -0300

    Added Game Service Tests

---
+++ src/services/game.service.test.ts
@@ -0,0 +1,65 @@
+import { expect } from 'chai';
+
+describe('Game Service', () => {
+
+    describe('#create()', function() {
+        it('Must be possible to create a new game', async function() {
+            throw new Error("fail");
+        });
+
+        it('the game must start with player X', async function() {
+            throw new Error("fail");
+        });
+
+        it('the game must start without a winner', async function() {
+            throw new Error("fail");
+        });
+      });
+
+      describe('#play()', function() {
+        it('should be possible to play a game created', async function() {
+            throw new Error("fail");
+        });
+
+        it('should be switched from O and X', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in all columns of row A', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in all columns of row B', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in all columns of row C', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in the first column of row A, B and C', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in the second column of row A, B and C', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in the third column of row A, B and C', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must be the winner if there is X in the first column of row A, the second column of row B and the third columns of row C', async function() {
+            throw new Error("fail");
+        });
+
+        it('X must have a maximum of 5 moves', async function() {
+            throw new Error("fail");
+        });
+
+        it('It must be possible to tie', async function() {
+            throw new Error("fail");
+        });
+      });
+
+});
